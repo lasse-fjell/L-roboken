0909fa12d90b2dbf6498d4cde9edf683336f7f77
Lars Brolén <lars.brolen@telia.com>
2011-10-05 14:36:15 +0200

    Kapitel 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LRoboken::Application.config.secret_token = 'fb5ccb59eaefa50ba22a4103d4eaec5552ce15523ea1dd1781eaef5f45a85e6080a90ff47b24f44d6d4f6899d533520f83e59acbbdecc3d0f8df2d230a231ab2'
